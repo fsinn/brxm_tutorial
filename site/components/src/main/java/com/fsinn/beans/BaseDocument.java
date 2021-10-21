9c73095a54d74f2e9da3cb724baca8ae87b4b038
Morathii <morathii@icloud.com>
2021-10-21 13:25:00 +0200

    Initial commit

---
+++ site/components/src/main/java/com/fsinn/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.fsinn.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="brxm_tutorial:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
