5e4cae3bd95accca3bd3bb0fed4283df16c4290a
Morathii <morathii@icloud.com>
2021-10-25 16:25:35 +0200

    Configure Image Variants

---
+++ site/components/src/main/java/com/fsinn/beans/Imageset.java
@@ -0,0 +1,10 @@
+package com.fsinn.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
+
+@HippoEssentialsGenerated(internalName = "brxm_tutorial:imageset")
+@Node(jcrType = "brxm_tutorial:imageset")
+public class Imageset extends HippoGalleryImageSet {
+}
---
+++ site/components/src/main/java/com/fsinn/beans/Simpledocument.java
@@ -0,0 +1,9 @@
+package com.fsinn.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+
+@HippoEssentialsGenerated(internalName = "brxm_tutorial:simpledocument")
+@Node(jcrType = "brxm_tutorial:simpledocument")
+public class Simpledocument extends BaseDocument {
+}
